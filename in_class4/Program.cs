4dc1d54287d7f386b447e295f360f24fbcea0c77
lime-owl <tomkferris@gmail.com>
2019-11-19 15:38:36 -0800

    menus for both species, next refactor this into a menu class

--- in_class4/Program.cs
+++ in_class4/Program.cs
@@ -26,13 +26,68 @@ namespace in_class4
                 String userOp = Console.ReadLine();
                 gamePlay = userOp;
 
-                if (userOp == "3")
+               if (userOp == "3") 
                 {
                     Console.WriteLine("Goodbye");
                 }
+                else if (userOp == "1")
+                {
+                    monkeySelect();
+                    Console.ReadLine();
+                }
+                else if (userOp == "2")
+                {
+                    bearSelect();
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("");
+                    Console.WriteLine("Please enter a valid selection.");
+                    userOp = Console.ReadLine();
 
+                }
             } while (gamePlay != "3");
         }
 
+        public static void monkeySelect()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Monkey Menu: ");
+            Console.WriteLine("================");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Species: ");
+            Console.WriteLine("-------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("1.Squirrel");
+            Console.WriteLine("2.Howler");
+            Console.WriteLine("3.Colobus");
+
+            Console.WriteLine("");
+            Console.WriteLine("Select: ");
+        }
+
+
+        public static void bearSelect()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Bear Menu: ");
+            Console.WriteLine("================");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Species: ");
+            Console.WriteLine("-------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("1.Polar");
+            Console.WriteLine("2.Black");
+            Console.WriteLine("3.Grizzly");
+
+            Console.WriteLine("");
+            Console.WriteLine("Select: ");
+        }
     }
 }
