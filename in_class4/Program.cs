390168e25f2ada684d1959623511c1051ac84810
lime-owl <tomkferris@gmail.com>
2019-11-19 15:21:42 -0800

    Simple gameplay loop

--- in_class4/Program.cs
+++ in_class4/Program.cs
@@ -6,7 +6,33 @@ namespace in_class4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string gamePlay = "";
+
+            do
+            {
+                Console.WriteLine("================");
+                Console.WriteLine("Zoo Menu Planner");
+                Console.WriteLine("================");
+                Console.WriteLine("");
+                Console.WriteLine("Select species: ");
+                Console.WriteLine("");
+                Console.WriteLine("1. Monkey");
+                Console.WriteLine("2. Bear");
+                Console.WriteLine("3. Quit");
+                Console.WriteLine("");
+                Console.WriteLine("Select: ");
+
+                String userOp = Console.ReadLine();
+                gamePlay = userOp;
+
+                if (userOp == "3")
+                {
+                    Console.WriteLine("Goodbye");
+                }
+
+            } while (gamePlay != "3");
         }
+
     }
 }
