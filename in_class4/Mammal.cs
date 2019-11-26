1e9d0e6daf7a44e1e024c0213687155f0cf4b703
lime-owl <55164193+lime-owl@users.noreply.github.com>
2019-11-26 15:37:43 -0800

    final parent "mammal" class

--- in_class4/Mammal.cs
+++ in_class4/Mammal.cs
@@ -1,17 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
-namespace in_class4
+namespace Zoo_Console
 {
-    class Mammal
+    abstract class Mammal
     {
-        public string name;
+       public string MammalName { get; set; }
+       public string SpeciesName { get; set; }
+       public string Weight { get; set; }
+       public string Note { get; set; }
+       public string FeedNote { get; set; }
+       public string Diet { get; set; }
 
-        public Mammal(string name)
+        public Mammal (string mname, string note, string feedNote, string diet, string speciesName, string weight)
         {
-            this.name = name;
+            MammalName = mname;
+            SpeciesName = speciesName;
+            Diet = diet;
+            Weight = weight;
+            Note = note;
+            FeedNote = feedNote;
         }
-
+        public string GetServing(string w)
+        {
+            decimal convertVal = decimal.Parse(w);
+            decimal total = decimal.Multiply(convertVal, 0.008m);
+            return total.ToString(CultureInfo.InvariantCulture);
+        }
     }
 }
